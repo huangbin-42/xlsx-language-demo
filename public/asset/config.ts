51f24b302b8f9dcd54965cdac55e6aec3ccef58f
Marco <finghtinghb42@gmail.com>
2023-11-06 16:48:46 +0800

    feat: 新增asset翻译

---
+++ public/asset/config.ts
@@ -0,0 +1,91 @@
+const config = {
+    noFoundTest: '***guass***',
+    initKey: 'web3.assets.',
+    contrastLangIndex: 4,
+    defaultValueIndex: 4,
+    fromXlsxPath: '/Users/a605/Desktop/code/xlsx-language-demo/public/asset/Web3  资产中心、DApps.xlsx',
+    translate: [
+        {
+            targetLang: 'zh',
+            targetIndex: 2,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/zh/asset.json`,
+        },
+        {
+            targetLang: 'zh-TW',
+            targetIndex: 3,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/zh-TW/asset.json`,
+        },
+        {
+            targetLang: 'en',
+            targetIndex: 4,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/en/asset.json`,
+        },
+        {
+            targetLang: 'pt',
+            targetIndex: 5,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/pt/asset.json`,
+        },
+        {
+            targetLang: 'pt-BR',
+            targetIndex: 5,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/pt-BR/asset.json`,
+        },
+        {
+            targetLang: 'fr',
+            targetIndex: 6,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/fr/asset.json`,
+        },
+        {
+            targetLang: 'es',
+            targetIndex: 7,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/es/asset.json`,
+        },
+        {
+            targetLang: 'ru',
+            targetIndex: 8,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/ru/asset.json`,
+        },
+        {
+            targetLang: 'vn',
+            targetIndex: 9,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/vn/asset.json`,
+        },
+        {
+            targetLang: 'de',
+            targetIndex: 10,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/de/asset.json`,
+        },
+        {
+            targetLang: 'th',
+            targetIndex: 11,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/th/asset.json`,
+        },
+        {
+            targetLang: 'id',
+            targetIndex: 12,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/id/asset.json`,
+        },
+        {
+            targetLang: 'uk',
+            targetIndex: 13,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/uk/asset.json`,
+        },
+        {
+            targetLang: 'tr',
+            targetIndex: 14,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/tr/asset.json`,
+        },
+        {
+            targetLang: 'ja',
+            targetIndex: 15,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/ja/asset.json`,
+        },
+        {
+            targetLang: 'ar',
+            targetIndex: 16,
+            outPath: `/Users/a605/Desktop/web3next/apps/asset/public/locales/ar/asset.json`,
+        },
+    ],
+}
+
+export default config
--- translate.config.ts
+++ translate.config.ts
@@ -1,4 +1,4 @@
 import guassConfig from './public/guass/config'
+import assetConfig from './public/asset/config'
 
-
-export default guassConfig
+export default assetConfig
