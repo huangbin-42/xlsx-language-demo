81b1f89bbc29e24c81cf19a3f6ddccbb8db77ea3
Marco <finghtinghb42@gmail.com>
2023-11-06 15:46:03 +0800

    📃 docs:新增猜价格翻译文案

---
+++ public/guass/config.ts
@@ -0,0 +1,56 @@
+const config = {
+    noFoundTest: '***guass***',
+    initKey: '',
+    contrastLangIndex: 1,
+    defaultValueIndex: 3,
+    fromXlsxPath: '/Users/a605/Desktop/code/xlsx-language-demo/public/guass/价格竞猜活动改版 页面文案.xlsx',
+    translate: [
+        {
+            targetLang: 'zh',
+            targetIndex: 1,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_zh.json`,
+        },
+        {
+            targetLang: 'tw',
+            targetIndex: 2,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_tw.json`,
+        },
+        {
+            targetLang: 'en',
+            targetIndex: 3,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_en.json`,
+        },
+        {
+            targetLang: 'ja',
+            targetIndex: 4,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_ja.json`,
+        },
+        {
+            targetLang: 'br',
+            targetIndex: 5,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_br.json`,
+        },
+        {
+            targetLang: 'tr',
+            targetIndex: 6,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_tr.json`,
+        },
+        {
+            targetLang: 'ru',
+            targetIndex: 7,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_ru.json`,
+        },
+        {
+            targetLang: 'vn',
+            targetIndex: 8,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_vn.json`,
+        },
+        {
+            targetLang: 'id',
+            targetIndex: 8,
+            outPath: `/Users/a605/Desktop/code/act-guess-price-web/src/lang/lang_id.json`,
+        },
+    ],
+}
+
+export default config
---
+++ translate.config.ts
@@ -0,0 +1,4 @@
+import guassConfig from './public/guass/config'
+
+
+export default guassConfig
