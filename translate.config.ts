cb60807a9bfa0744e92a042fd90b8edeff287702
Marco <finghtinghb42@gmail.com>
2023-11-27 16:56:37 +0800

    1

--- translate.config.ts
+++ translate.config.ts
@@ -1,6 +1,7 @@
 import guassConfig from './public/guass/config'
 import assetConfig from './public/asset/config'
 import homeConfig from './public/home/config'
+import newTrandingFairConfig from './public/new-tranding-fair/config'
 
 
-export default assetConfig
+export default newTrandingFairConfig
