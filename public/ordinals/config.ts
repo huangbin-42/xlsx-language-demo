4814781d2f445a232c80da3d3ee76b566afc3c92
Marco <finghtinghb42@gmail.com>
2023-11-29 13:37:02 +0800

    feat: 市场

---
+++ public/ordinals/config.ts
@@ -0,0 +1,91 @@
+const config = {
+    noFoundTest: '----------------------',
+    initKey: 'web3.ordinals.',
+    contrastLangIndex: 3,
+    defaultValueIndex: 1999,
+    fromXlsxPath: '/Users/a605/Desktop/code/xlsx-language-demo/public/ordinals/ordinals翻译导入用文档.xlsx',
+    translate: [
+        {
+            targetLang: 'zh',
+            targetIndex: 1,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/zh/ordinals.json`,
+        },
+        {
+            targetLang: 'zh-TW',
+            targetIndex: 2,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/zh-TW/ordinals.json`,
+        },
+        {
+            targetLang: 'en',
+            targetIndex: 3,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/en/ordinals.json`,
+        },
+        {
+            targetLang: 'pt',
+            targetIndex: 4,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/pt/ordinals.json`,
+        },
+        {
+            targetLang: 'pt-BR',
+            targetIndex: 4,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/pt-BR/ordinals.json`,
+        },
+        {
+            targetLang: 'fr',
+            targetIndex: 5,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/fr/ordinals.json`,
+        },
+        {
+            targetLang: 'es',
+            targetIndex: 6,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/es/ordinals.json`,
+        },
+        {
+            targetLang: 'ru',
+            targetIndex: 7,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/ru/ordinals.json`,
+        },
+        {
+            targetLang: 'vn',
+            targetIndex: 8,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/vn/ordinals.json`,
+        },
+        {
+            targetLang: 'de',
+            targetIndex: 9,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/de/ordinals.json`,
+        },
+        {
+            targetLang: 'th',
+            targetIndex: 10,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/th/ordinals.json`,
+        },
+        {
+            targetLang: 'id',
+            targetIndex: 11,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/id/ordinals.json`,
+        },
+        {
+            targetLang: 'uk',
+            targetIndex: 12,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/uk/ordinals.json`,
+        },
+        {
+            targetLang: 'tr',
+            targetIndex: 13,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/tr/ordinals.json`,
+        },
+        {
+            targetLang: 'ja',
+            targetIndex: 14,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/ja/ordinals.json`,
+        },
+        {
+            targetLang: 'ar',
+            targetIndex: 15,
+            outPath: `/Users/a605/Desktop/web3next/apps/ordinals/public/locales/ar/ordinals.json`,
+        },
+    ],
+}
+
+export default config
--- translate.config.ts
+++ translate.config.ts
@@ -2,6 +2,7 @@ import guassConfig from './public/guass/config'
 import assetConfig from './public/asset/config'
 import homeConfig from './public/home/config'
 import newTrandingFairConfig from './public/new-tranding-fair/config'
+import ordinalsConfig from './public/ordinals/config'
 
 
-export default newTrandingFairConfig
+export default ordinalsConfig
