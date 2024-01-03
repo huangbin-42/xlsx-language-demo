e78f9a76eb0207c9edf2d71166fec95a6714576f
Marco <finghtinghb42@gmail.com>
2024-01-03 11:17:11 +0800

    1

---
+++ public/price-detail-website/config.ts
@@ -0,0 +1,101 @@
+const config = {
+    noFoundTest: '----------------------',
+    initKey: '',
+    contrastLangIndex: 3,
+    defaultValueIndex: 1999,
+    fromXlsxPath: '/Users/a605/Desktop/Plug/xlsx-language-demo/public/price-detail-website/币种信息多语言翻译.xlsx',
+    translate: [
+        {
+            targetLang: 'zh',
+            targetIndex: 1,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_zh.json`,
+        },
+        {
+            targetLang: 'zh-TW',
+            targetIndex: 2,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_tw.json`,
+        },
+        {
+            targetLang: 'en',
+            targetIndex: 3,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/messages/next_price_detail.json`,
+        },
+        {
+            targetLang: 'en',
+            targetIndex: 3,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_it.json`,
+        },
+        {
+            targetLang: 'en',
+            targetIndex: 3,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_nl.json`,
+        },
+        {
+            targetLang: 'pt',
+            targetIndex: 4,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_pt.json`,
+        },
+        {
+            targetLang: 'pt-BR',
+            targetIndex: 4,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_br.json`,
+        },
+        {
+            targetLang: 'fr',
+            targetIndex: 5,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_fr.json`,
+        },
+        {
+            targetLang: 'es',
+            targetIndex: 6,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_es.json`,
+        },
+        {
+            targetLang: 'ru',
+            targetIndex: 7,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_ru.json`,
+        },
+        {
+            targetLang: 'vn',
+            targetIndex: 8,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_vn.json`,
+        },
+        {
+            targetLang: 'de',
+            targetIndex: 9,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_de.json`,
+        },
+        {
+            targetLang: 'th',
+            targetIndex: 10,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_th.json`,
+        },
+        {
+            targetLang: 'id',
+            targetIndex: 11,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_id.json`,
+        },
+        {
+            targetLang: 'uk',
+            targetIndex: 12,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_uk.json`,
+        },
+        {
+            targetLang: 'tr',
+            targetIndex: 13,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_tr.json`,
+        },
+        {
+            targetLang: 'ja',
+            targetIndex: 14,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_ja.json`,
+        },
+        {
+            targetLang: 'ar',
+            targetIndex: 15,
+            outPath: `/Users/a605/Desktop/Plug/smartling_website/translated/next_price_detail/next_price_detail_ar.json`,
+        },
+    ],
+}
+
+export default config
--- translate.config.ts
+++ translate.config.ts
@@ -5,6 +5,7 @@ import newTrandingFairConfig from './public/new-tranding-fair/config'
 import ordinalsConfig from './public/ordinals/config'
 import weekendConfig from './public/weekend/config'
 import priceDetailConfig from './public/price-detail/config'
+import priceDetailWebsiteConfig from './public/price-detail-website/config'
 
 
-export default priceDetailConfig
+export default priceDetailWebsiteConfig
